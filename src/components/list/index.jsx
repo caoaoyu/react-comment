d4f79e1db6cd91e1780187809b2eac69fe6f2d4a
Aoyo <312318138@qq.com>
2018-11-02 10:04:51 +0800

    整理CSS

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import Item from '../item/index';
 import None from '../general/none/index';
 import PropTypes from 'prop-types';
+import './index.css';
 
 const h_list = (array) => {
-	return array.map((e,i) => {
-		console.log(e)
-		return <Item key={`comment-${i}`} data={e}/>
-	})
+	return array.map((e,i) => <Item key={`comment-${i}`} data={e}/>)
 }
 const List = ({ comments }) => {
 	console.log(comments)
