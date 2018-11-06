979c1a650585a375f27ce9516b6360cef46c6005
Aoyo <312318138@qq.com>
2018-11-06 15:27:25 +0800

    feat：time format

--- util/index.js
+++ util/index.js
@@ -6,23 +6,41 @@ export const timeToDate = (time) => {
 	let h = date.getHours();
 	let minutes = date.getMinutes();
 	let s = date.getSeconds();
-	// return {
-	// 	y,
-	// 	m,
-	// 	d,
-	// 	h,
-	// 	minutes,
-	// 	s
-    // };
-    return `${y}-${m <10 ? `0${m}` : m}-${d <10 ? `0${d}` : d} ${h <10 ? `0${h}` : h} ：${minutes <10 ? `0${minutes}` : minutes}`
+	const before_format = timeToFormat({
+		y,
+		m,
+		d,
+		h,
+		minutes,
+		s
+	});
+	return before_format;
+	// return `${y}-${m < 10 ? `0${m}` : m}-${d < 10 ? `0${d}` : d} ${h < 10 ? `0${h}` : h} ：${minutes < 10
+	// 	? `0${minutes}`
+	// 	: minutes}`;
 };
 
-// export const timeToDate = (time) => {
-// 	let before = timeToNumber(time);
-// 	let now = timeToNumber(new Date().getTime());
-//     const { y, m, d, h, minutes, s } = before;
-//     let year_date = `${y}-${m <10 ? `0${m}` : m}-${d <10 ? `0${d}` : d}`
-//     let m_date = `${m <10 ? `0${m}` : m}-${d <10 ? `0${d}` : d}`
-//     if (y < now.y) return year_date;
-//     if(m > now.y) return 
-// };
+export const timeToFormat = (before, time) => {
+	let now = new Date();
+	const { y, m, d, h, minutes, s } = before;
+	let now_y = now.getFullYear();
+	let now_m = now.getMonth() + 1;
+	let now_d = now.getDate();
+	let now_h = now.getHours();
+	let now_minutes = now.getMinutes();
+	let now_s = now.getSeconds();
+
+	const year = `${y}-${m < 10 ? `0${m}` : m}-${d < 10 ? `0${d}` : d}`;
+	const month_date = `${now_m - m} 月前`;
+	const day_date = `${now_d - d} 天前`;
+	const hour_date = `${now_h - h} 小时前`;
+	const minutes_date = `${now_minutes - minutes} 分钟前`;
+	const s_date = `${(now_s - s) + 1} 秒前`;
+
+	if (y > now_y) return year;
+	if (m > now_m) return month_date;
+	if (d < now_d) return day_date;
+	if (h < now_h) return hour_date;
+	if (minutes < now_minutes) return minutes_date;
+	if (s <= now_s) return s_date;
+};
